df99c04e2432ba713f62fec2edeb4d865c1b780a
Nithin <nithinkatkam504106@gmail.com>
2023-12-11 13:59:58 +0530

    done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
-// import { getUnixTime } from 'date-fns/fp';
+import { ethers } from 'ethers';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useSession, signIn, signOut } from 'next-auth/react';
@@ -8,30 +8,13 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 import { useEnsAddress } from 'wagmi';
 import { useEnsAvatar, useEnsName } from 'wagmi';
 
-const sampleSchema = {
-  // url: 'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json/KYCAgeCredential-v2.json',main schema
-  url: 'https://schema.dock.io/Brithday-V1-1701803023413.json',
-  name: 'Brithday',
-  populateFunc() {
-      // NOTE: the attributes returned here MUST match the schema
-      return {
-        date: 23
-      };
-    }
-};
-  // https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld
+ 
 export default function Home() {
-  // const twitterClient = new TwitterApi('AAAAAAAAAAAAAAAAAAAAAGuyrQEAAAAAKY2Au7vQCg%2FdOX2zTd3WM8YlxDY%3DwSXzoTv63zOIkzvmr7l96l1fyOa3hqPyBIM92TokwZfVosCnxe');
-  // const username = 'Nithinkd';
   const { data: session } = useSession();
   const [issuerName, setIssuerName] = useState();
   const [issuerProfile, setIssuerProfile] = useState();
-  const [credentialData, setCredentialData] = useState(
-    {
-      schema: sampleSchema.url,
-      subject: {}
-    });
 
+    const [account, setAccount] = useState(null);
     const [dids, setDids] = useState('');
   const [claimQR, setClaimQR] = useState('');
   const [didInfos, setDidInfos] = useState(null);
@@ -44,8 +27,36 @@ export default function Home() {
     console.log(data);
     setIssuerProfile(data);
   }
+  const address = '0xa2a9055a014857d6c1e8f1bdd8682b6459c5fa85';
+  const ensNames = useEnsName({ address, chainId: 11155111 });
+  const ensAvatars = useEnsAvatar({ name: ensName.data, chainId: 11155111 });
 
+    const connectToMetaMask = async () => {
+      try {
+        const [userAccount] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(userAccount);
+      } catch (error) {
+        console.error('Error connecting to MetaMask:', error.message);
+      }
+    };
+ // https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld
+ const sampleSchema = {
+  // url: 'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json/KYCAgeCredential-v2.json',main schema
+  url: 'https://schema.dock.io/ENS-V1-1702277612452.json',
+  name: 'ENS',
+  populateFunc() {
+      return {
+        ens: ensNames.data,
+        twitter: session.user.name
+      };
+    }
+};
 
+    const [credentialData, setCredentialData] = useState(
+      {
+        schema: sampleSchema.url,
+        subject: {}
+      });
   const handleGetDID = async () => {
     try {
       const response = await axios.get(`https://api-testnet.dock.io/dids/${dids}`, {
@@ -75,9 +86,7 @@ export default function Home() {
     console.log(data);
     setClaimQR(data.qrUrl);
   }
-  const address = '0xA2a9055A014857d6c1e8f1BDD8682B6459C5Fa85';
-  const ensNames = useEnsName({ address, chainId: 11155111 });
-  const ensAvatars = useEnsAvatar({ name: ensName.data, chainId: 11155111 });
+
 
   if (!issuerProfile) {
     return (
@@ -87,6 +96,7 @@ export default function Home() {
         display: 'flex',
         alignItems: 'center',
         gap: '0.5rem',
+        margin: '23px'
       }}
       className='m-2'
     >
@@ -94,55 +104,41 @@ export default function Home() {
         src={ensAvatars.data || 'alt'}
         style={{ width: '2rem', height: '2rem', objectFit: 'cover' }}
       />
+      <div className='text-center font-semibold text-3xl mt-12'>
+      ENS PROFILE:
       <span>
         {ensName.isError
           ? 'Error loading name'
           : ensNames.isLoading
           ? 'Loading...'
-          : ensNames.data || 'No name set'}
+          : ensNames.data || 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.FKRDzeTLhDAPOrLaJ9OesAHaHa%26pid%3DApi&f=1&ipt=44c4121667e2a0eb70f227fc5945f27154e60b3acc3348d04dfbbc4085882676&ipo=images'}
       </span>
+      </div>
     </div>
       <p>
        {!session && <>
             Not signed in <br/>
-            <button onClick={() => signIn()}>Sign in</button>
+            <button className='text-center bg-zinc-600 p-2 m-3' onClick={() => signIn()}>Sign in Twitter</button>
           </>}
           {session && <>
             Signed in as {session.user.name} <br/>
-            <button onClick={() => signOut()}>Sign out</button>
+            <button className='p-2 text-center bg-red-200 mt-2'onClick={() => signOut()}>Sign out</button>
 
-            <div>Address: {ensName.data}</div>
+            {/* <div>Address: {ensName.data}</div> */}
           </>}
 
           </p>
 
-        <div className="flex flex-col-reverse lg:flex-row lg:h-screen">
-          <div
-            className="lg:flex w-full lg:w-1/2 justify-around items-center text-center bg-zinc-900"
-            style={{
-              flexShrink: 0,
-              background: 'url(/bg.jpg)',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-            }}>
-            <div
-              className="w-full mx-auto px-0 py-10 flex-col items-center space-y-6"
-              style={{
-                maxWidth: '400px',
-                width: '100%',
-              }}>
-              <h1 className="text-white font-bold text-4xl">
-                Dock  Polygon ID Demo
-              </h1>
-
-              <p className="text-white mt-1">
-                In this interactive demo you will see how Dock&apos;s technology can be used to issue a
-                Verifiable Credential with Polygon ID.
-              </p>
-              <br />
-              <br />
-            </div>
-          </div>
+          <div className='m-3'>
+      <h1>MetaMask Connect</h1>
+      {account ? (
+        <p>Connected Account: {account}</p>
+      ) : (
+        <button className='p-2 text-center bg-red-200 mt-1' onClick={() => connectToMetaMask()}>Connect to MetaMask</button>
+      )}
+    </div>
+        <div className="flex font-bold lg:h-screen mt-20 m-5">
+         Twitter+ ENS+ POLYGON VC
           <div
             className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8"
             style={{ width: '100%' }}>
@@ -151,7 +147,7 @@ export default function Home() {
                 onSubmit={handleGenerateProfileSubmit}
                 className="p-5"
                 style={{ maxWidth: '500px', margin: '0 auto' }}>
-                <h1 className="text-gray-800 font-bold text-2xl mb-6">Create a Polygon Issuer</h1>
+                <h1 className="text-gray-800 font-bold text-2xl mb-6">Get DID</h1>
                 <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
                   <input
                     id="name"
@@ -159,6 +155,7 @@ export default function Home() {
                     name="name"
                     placeholder="Issuer Name"
                     value={issuerName}
+                    required={ensName.data}
                     onChange={(event) =>
                       setIssuerName(event.target.value)}
                   />
@@ -181,7 +178,7 @@ export default function Home() {
         value={dids}
         onChange={(e) => setDids(e.target.value)}
       />
-      <button onClick={handleGetDID}>Get DIDsss Information</button>
+      <button onClick={handleGetDID}>Get Your Did Info</button>
       {didInfos && (
         <div>
           <h2>DID Information</h2>
@@ -199,7 +196,7 @@ export default function Home() {
       <>
         <div className="flex flex-col-reverse lg:flex-row lg:h-screen">
           <div
-            className="lg:flex w-full lg:w-1/2 justify-around items-center text-center bg-zinc-900"
+            className="lg:flex w-full lg:w-1/2 justify-around items-center text-center bg-zinc-200"
             style={{
               flexShrink: 0,
               background: 'url(/bg.jpg)',
@@ -213,12 +210,8 @@ export default function Home() {
                 width: '100%',
               }}>
               <h1 className="text-white font-bold text-4xl">
-                KYC Age Credential Example
+               ENS+TWITTER VC
               </h1>
-
-              <p className="text-white mt-1">
-                We&apos;ll issue a KYC Age Credential for which we need a birthdate.
-              </p>
               <br />
               <br />
             </div>
@@ -231,31 +224,16 @@ export default function Home() {
                 onSubmit={handleCreateCredentialRequest}
                 className="p-5"
                 style={{ maxWidth: '500px', margin: '0 auto' }}>
-                <h1 className="text-gray-800 font-bold text-2xl mb-6">Enter a date of birth</h1>
+                <h1 className="text-gray-300 font-bold text-2xl mb-6">Confirm your vc</h1>
                 <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
-                  <DatePicker
-                    id="dob"
-                    className=" pl-2 w-full outline-none border-none"
-                    name="dob"
-                    selected={credentialData.subject.dob}
-                    onSelect={(date) => {
-                      setCredentialData({
-                        ...credentialData,
-                        subject: {
-                          ...credentialData.subject,
-                          dob: date
-                        }
-                      });
-                    }
-                  }
-                  />
+     
                 </div>
 
                 <button
                   type="submit"
-                  disabled={!credentialData.subject.dob}
+                  // disabled={!credentialData.subject.dob}
                   className="block w-full bg-blue-600 mt-5 py-2 rounded-full hover:bg-blue-700 hover:-translate-y-1 transition-all duration-250 text-white font-semibold mb-2">
-                  Create Credential Request
+                  Confirm VC
                 </button>
 
               </form>
